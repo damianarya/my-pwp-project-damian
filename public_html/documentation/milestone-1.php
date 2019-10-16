da7016a1295e8148d3a6e89486a834fdc4bb7b0b
Damian Arya <dahakcyrus@yahoo.com>
2019-10-16 16:22:28 -0600

    -m modifyed purpose, audience and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,15 @@
 	<meta charset="UTF-8">
 </head>
 <body>
-
+<p>
+	purpose; I would like my PWP to help me learn how to create a web site that works well and looks good.
+<br>
+	audience;My goal audience is comprised of people who are interested in ghost hunting
+	and the paranormal. My audience is comprised of a very diverse crowd ranging
+	younge to old, rich to poor ect. hopfully a few TV executives as well.
+<br>
+	Goal; To serve as a gallery to showcase and promote my work in the feild as well as the (cercumstantial)
+	eveidence my team and I collect durring our ventures.
+</p>
 </body>
 </html>
