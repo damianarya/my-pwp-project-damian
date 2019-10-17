af7e5435c266e40fc3a852738a5fcec6601800a7
Damian Arya <dahakcyrus@yahoo.com>
2019-10-16 20:23:42 -0600

    added to the frustrations and occupation fields

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -20,5 +20,56 @@
 	<br>
 	the (circumstantial) evidence my team and I collect during our ventures and investigations.
 </p>
+<br>
+<br>
+<p>
+	<h1> Persona</h1>
+<br>
+<img src="pwpimage.jpg">
+</p>
+<br>
+<p>
+	<strong>Name</strong>; Debbie Housifgabe
+	<br>
+	<br>
+	<strong>age</strong>; 40
+	<br>
+	<br>
+	<strong>profession</strong>; Housewife and part time caretaker. I also dabble in continuing education when the subject
+	<br>
+	suits me. My hobbys include ghost hunting in the summer and excessive holiday decorating but mostly I watch every
+	<br>
+	episode of the SYFY networks paranormal investigation shows.
+	<br>
+	<br>
+	<strong>Technology</strong>; I am ok with most computers. I can type one hundred and fifty words per minuet but I
+	<br>
+	dont know alot about these new devices and updates coming out every other month. I can use Youtube, microsoft word
+	<br>
+	and most web sites like ebay or facebook with ease but I usually let my kids do the really technical stuff like
+	<br>
+	programming the microwave clock and all the software updates.
+	<br>
+	<br>
+	<strong>Attitudes and Behaviors</strong>; Debbie has a lot of time on her hands what with her kids in high school and her
+	<br>
+	partner at work all day. She spends a lot of time trying to stay busy. Debbie can get board pretty easily but if
+	<br>
+	somthing grabs her attention she can focus on it all day and really dive into it. Debbie likes things she can put
+	<br>
+	down and forget about for a long time but pick back up right where she left off, like an Ann Rice book or that pizza
+	<br>
+	in the fridge. Oh and she pritty sure she saw a ghost once.
+	<br>
+	<br>
+	<strong>Frustrations and Needs</strong>; Debbie gets so board, her shows dont air any new episodes in the summer so
+	<br>
+	she needs something to occupy her time. She really misses her shows but with no new content she is forced to look elsewhere
+	<br>
+	for her paranormal curiosity needs. She also wants to know if the ghost she saw compares to the ones other people have seen.
+	<br>
+	<br>
+	<strong>Goals</strong>;
+</p>
 </body>
 </html>
