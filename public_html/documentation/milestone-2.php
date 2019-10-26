11811153f5c0b26e584ecc57a485c15543e67759
Damian Arya <dahakcyrus@yahoo.com>
2019-10-26 13:40:22 -0600

    put the finishing touches on the content strategy.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,1 +1,32 @@
-<?php
+<!DOCTYPE>
+<html lang="en">
+<head>
+	<title>pwp project milestone two</title>
+	<meta charset="UTF-8">
+</head>
+<body>
+<p>My site will be one page and consist of the following sections.<br>
+<br><strong>Welcome</strong> this will be a large picture of my team and I.<br>
+	I intend on putting an official logo and possibly a search bar at the top above the graphic image.<br>
+<br>
+	<div>
+	<strong>About us</strong> This section will have an image for every current member of the team as well as a short <br>
+	bio in text about eash person and what they bring to the team as a whole. It may include more in depth information such<br>
+	as how long they have been on the team or which hunts they have been a part of. The background color may be different<br>
+	from the preceding and following sections.<br>
+	<br>
+	<strong>Portfolio</strong> This will be my most in depth and interactive section of the page. I intend to put a scrollable<br>
+	picture gallery at the top of the section with an image from each hunt my team has done. underneath that i plan to put a<br>
+	discriptive text section that details the hunt currently displayed above. this will require that the text change along<br>
+	with the pictures above as the user scrolls from one image to the next.<br>
+	<br>
+	<strong>Contact</strong>This section will be set up for fans to contact the group. There will be an email option that will<br>
+	go to a group email adress. We will also have a "join the team" option for people who wish to join the fan club by adding<br>
+	there name and email for official updates on the teams new adventures. At the bottom of the page there will be a few links<br>
+	to other social media sites the group maintains and possibly a youtube link to a page that showcases our vidios. This section<br>
+	will be a different color from the preceding one.
+</div>
+</p>
+
+
+</body>
