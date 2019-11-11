7b3bb1ce847935f611d5621cedf7503ebb588544
Damian Arya <dahakcyrus@yahoo.com>
2019-11-11 13:44:29 -0700

    doing code along

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// add this into coposer.json file instead
+{
+	"require": {
+	"google/recaptcha": "~1.1",
+    "mailgun/mailgun-php": "@stable",
+    "kriswallsmith/buzz": "@stable",
+    "nyholm/psr7": "@stable",
+    "swiftmailer/swiftmailer": "@stable"
+
+  }
+}
